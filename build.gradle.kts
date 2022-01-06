e556e5c6da078dc56300c9eb52cc7c252c961b42
Valerie Wagner <vwagner@figure.com>
2022-01-05 18:37:40 -0700

    Publish source and javadocs to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ repositories {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
 }
 
 val projectVersion = project.property("version")?.takeIf { it != "unspecified" } ?: "1.0-SNAPSHOT"
